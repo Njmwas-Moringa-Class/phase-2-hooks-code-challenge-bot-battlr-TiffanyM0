e1652437d8f6a48245e251cd7d1704eb21fe49d6
TIFFANY MAINA <tiffmaina@gmail.com>
2023-12-02 22:57:49 +0300

    BotCollection display

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import BotCard from "./BotCard";
 
 function BotCollection() {
   // Your code here
+  // See profiles of all bots rendered in BotCollection.
+  
+  const [battleBot, setBattleBot] = useState([]);
+
+  useEffect(()=>{
+    fetch ("http://localhost:8002/bots")
+    .then ((resp)=>resp.json())
+    .then((data)=>{
+      setBattleBot(data)
+    })
+  }, []);
+  // console.log(battleBot)
   return (
     <div className="ui four column grid">
       <div className="row">
-        {/*...and here..*/}
-        Collection of all bots
+        {/* Collection of all bots */}
+        {battleBot.map((bot)=> <BotCard key={bot.id} bot={bot}/>)}
       </div>
     </div>
   );
--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,7 +1,12 @@
 import React from "react";
+// import BotCollection from "./BotCollection";
 
 function YourBotArmy() {
   //your bot army code here...
+
+  // Add an individual bot to my army by clicking on it. The selected bot should render in the YourBotArmy component.
+  // The bot can be enlisted only once. The bot does not disappear from the BotCollection.
+// the child botcollection passes bots to the parent, key{bot.id} can only go up once
 
   return (
     <div className="ui segment inverted olive bot-army">
